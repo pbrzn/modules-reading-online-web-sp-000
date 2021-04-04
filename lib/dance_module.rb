29e5a2f1069099b0c98357b53d02fdd0480068d0
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-04 16:23:03 +0000

    Automatically backed up by Learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,17 @@
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumping!"
+  end
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end
+  
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure dancing for you all."
+  end
+end
